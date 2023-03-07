088d5f396f80279b859ac13cb20bf7c4c970e875
Marouan <115103579+marouanal@users.noreply.github.com>
2023-03-07 17:44:32 +0000

    Add files via upload

---
+++ ShootMe bird/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Bird Shooter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bird Shooter")]
+[assembly: AssemblyCopyright("Copyright ©  Marouan Alouache")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("faca84ca-dd50-4d8c-b137-ceb9cc72719a")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
